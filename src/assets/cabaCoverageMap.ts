8071590c2789b2499a9ad003c8e6c571fbcfa804
polizelener-droid <polizelener@gmail.com>
2026-08-24 16:59:30 -0300

    Preparar imagen del mapa de cobertura

---
+++ src/assets/cabaCoverageMap.ts
@@ -0,0 +1,9 @@
+import chunk1 from './cabaCoverageMapChunk1';
+import chunk2 from './cabaCoverageMapChunk2';
+import chunk3 from './cabaCoverageMapChunk3';
+import chunk4 from './cabaCoverageMapChunk4';
+import chunk5 from './cabaCoverageMapChunk5';
+
+const cabaCoverageMap = `data:image/webp;base64,${chunk1}${chunk2}${chunk3}${chunk4}${chunk5}`;
+
+export default cabaCoverageMap;
