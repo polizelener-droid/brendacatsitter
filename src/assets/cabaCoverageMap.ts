543b497f0fc91a95a5499dbe922c35e42f52c89a
polizelener-droid <polizelener@gmail.com>
2026-08-24 17:32:39 -0300

    Servir mapa de cobertura como imagen estática

--- src/assets/cabaCoverageMap.ts
+++ src/assets/cabaCoverageMap.ts
@@ -1,9 +1,3 @@
-import chunk1 from './cabaCoverageMapChunk1';
-import chunk2 from './cabaCoverageMapChunk2';
-import chunk3 from './cabaCoverageMapChunk3';
-import chunk4 from './cabaCoverageMapChunk4';
-import chunk5 from './cabaCoverageMapChunk5';
-
-const cabaCoverageMap = `data:image/webp;base64,${chunk1}${chunk2}${chunk3}${chunk4}${chunk5}`;
+const cabaCoverageMap = '/caba-coverage-map.webp';
 
 export default cabaCoverageMap;
