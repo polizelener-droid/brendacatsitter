a475a8f589bccc806a38209e33166df436559e04
polizelener-droid <polizelener@gmail.com>
2026-09-09 23:32:21 -0300

    Connect admin2 route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ContentProvider } from './content/ContentContext';
 import { AdminApp } from './admin/AdminApp';
+import { Admin2Route } from './admin/Admin2Route';
 import { HomePage } from './pages/HomePage';
 import { BudgetPage } from './pages/BudgetPage';
 
@@ -15,23 +16,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <ContentProvider>
-              <HomePage />
-            </ContentProvider>
-          }
-        />
-        <Route
-          path="/presupuesto"
-          element={
-            <ContentProvider>
-              <BudgetPage />
-            </ContentProvider>
-          }
-        />
+        <Route path="/" element={<ContentProvider><HomePage /></ContentProvider>} />
+        <Route path="/presupuesto" element={<ContentProvider><BudgetPage /></ContentProvider>} />
         <Route path="/admin/*" element={<AdminApp />} />
+        <Route path="/admin2" element={<Admin2Route />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
