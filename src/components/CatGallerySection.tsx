a3634b0494cd78b70001440f3d97b159f8c60a48
polizelener-droid <polizelener@gmail.com>
2026-09-25 10:39:55 -0300

    Mostrar 12 gatos por fila en computadora

--- src/components/CatGallerySection.tsx
+++ src/components/CatGallerySection.tsx
@@ -142,7 +142,7 @@ export const CatGallerySection: React.FC = () => {
             </div>
           ) : (
             <>
-              <div className="grid grid-cols-2 gap-3 pr-1 sm:grid-cols-3 sm:gap-4 lg:grid-cols-4">
+              <div className="grid grid-cols-3 gap-2 pr-1 sm:grid-cols-4 sm:gap-3 lg:grid-cols-6 xl:grid-cols-12">
                 {visibleCats.map((cat: CatClient) => <CatCard key={cat.id} cat={cat} index={cats.indexOf(cat)} />)}
               </div>
 
